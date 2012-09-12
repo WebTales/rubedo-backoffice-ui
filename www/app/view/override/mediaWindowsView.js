0890d1a6599b932d9eec594be67f70445bf0f2f2
Julien Bourdin <julien.bourdin@webtales.fr>
2012-09-12 11:42:10 +0200

    Migrate from Sensha Architect 2.0 to 2.1

--- www/app/view/override/mediaWindowsView.js
+++ www/app/view/override/mediaWindowsView.js
@@ -1,10 +1,7 @@
 Ext.define('Rubedo.view.override.mediaWindowsView', {
-    requires: 'Rubedo.view.mediaWindowsView'
-}, function() {
-    Ext.override(Rubedo.view.mediaWindowsView, {
-        plugins:[
-                Ext.create('Ext.ux.DataView.DragSelector', {}),
-                Ext.create('Ext.ux.DataView.LabelEditor', {dataIndex: 'text'})
-            ]
-    });
+    override: 'Rubedo.view.mediaWindowsView',
+    plugins:[
+    Ext.create('Ext.ux.DataView.DragSelector', {}),
+    Ext.create('Ext.ux.DataView.LabelEditor', {dataIndex: 'text'})
+    ]
 });
