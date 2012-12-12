962c8b5daa59a6df8ffc298516f26efe94287a02
Alexandru Dobre <alexdobre89@gmail.com>
2012-12-12 11:23:48 +0100

    testing component

--- www/app/view/testingGround.js
+++ www/app/view/testingGround.js
@@ -21,8 +21,7 @@ Ext.define('Rubedo.view.testingGround', {
     id: 'testingGround',
     width: 959,
     layout: {
-        align: 'stretch',
-        type: 'vbox'
+        type: 'anchor'
     },
     title: 'Testing ground',
 
@@ -32,89 +31,19 @@ Ext.define('Rubedo.view.testingGround', {
         Ext.applyIf(me, {
             items: [
                 {
-                    xtype: 'form',
-                    flex: 1,
-                    autoScroll: true,
-                    layout: {
-                        align: 'stretch',
-                        type: 'vbox'
-                    },
-                    bodyPadding: 10,
-                    title: 'Upload test',
-                    dockedItems: [
-                        {
-                            xtype: 'toolbar',
-                            dock: 'bottom',
-                            listeners: {
-                                afterrender: {
-                                    fn: me.onToolbarAfterRender,
-                                    scope: me
-                                }
-                            }
-                        }
-                    ],
-                    items: [
-                        {
-                            xtype: 'container',
-                            flex: 1,
-                            id: 'dragload'
-                        }
-                    ]
+                    xtype: 'textareafield',
+                    anchor: '90%',
+                    fieldLabel: 'Label'
+                },
+                {
+                    xtype: 'textfield',
+                    anchor: '100%',
+                    fieldLabel: 'Label'
                 }
             ]
         });
 
         me.callParent(arguments);
-    },
-
-    onToolbarAfterRender: function(abstractcomponent, options) {
-        abstractcomponent.add(Ext.create('Ext.ux.upload.Button', {
-            text: 'Select files',
-            //singleFile: true,
-            plugins: [Ext.create("Ext.ux.upload.plugin.Window",{title:"test",height:300,width:300})
-            ],
-            uploader: 
-            {
-                url: 'image/put',
-                //uploadpath: '/Root/files',
-                autoStart: false,
-                max_file_size: '2020mb',			
-                drop_element: 'dragload',
-                statusQueuedText: 'Ready to upload',
-                statusUploadingText: 'Uploading ({0}%)',
-                statusFailedText: '<span style="color: red">Error</span>',
-                statusDoneText: '<span style="color: green">Complete</span>',
-
-                statusInvalidSizeText: 'File too large',
-                statusInvalidExtensionText: 'Invalid file type'
-            },
-            listeners: 
-            {
-                filesadded: function(uploader, files)								
-                {
-                    //console.log('filesadded');
-                    return true;
-                },
-
-                beforeupload: function(uploader, file)								
-                {
-                    //console.log('beforeupload');			
-                },
-
-                fileuploaded: function(uploader, file)								
-                {
-                    //console.log('fileuploaded');
-                },
-
-                uploadcomplete: function(uploader, success, failed)								
-                {
-                    //console.log('uploadcomplete');				
-                },
-                scope: this
-            }
-
-
-        }));
     }
 
 });
