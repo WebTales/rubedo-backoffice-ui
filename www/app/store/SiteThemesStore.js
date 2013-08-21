b71dbd0738182a187e4a5ed5c99377595f35c5ec
Alexandru Dobre <alexandru.dobre@webtales.fr>
2013-08-21 16:56:33 +0200

    store sync for custom themes with picker

--- www/app/store/SiteThemesStore.js
+++ www/app/store/SiteThemesStore.js
@@ -21,8 +21,9 @@ Ext.define('Rubedo.store.SiteThemesStore', {
         var me = this;
         cfg = cfg || {};
         me.callParent([Ext.apply({
+            isOptimised: true,
+            usedCollection: 'CustomThemes',
             autoLoad: false,
-            autoSync: true,
             storeId: 'SiteThemesStore',
             pageSize: 1000,
             proxy: {
