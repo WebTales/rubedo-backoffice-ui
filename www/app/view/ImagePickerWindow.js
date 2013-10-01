d58222f93a7db95bcf220cc2ae602f0838b86f89
Alexandru Dobre <alexandru.dobre@webtales.fr>
2013-10-01 15:54:56 +0200

    fix media picker

--- www/app/view/ImagePickerWindow.js
+++ www/app/view/ImagePickerWindow.js
@@ -152,11 +152,9 @@ Ext.define('Rubedo.view.ImagePickerWindow', {
                         }
                     },
                     filter: {
-                        type: 'combo',
-                        valueField: 'id',
-                        id:'chooseMTInPickerCombo',
-                        displayField: 'type',
-                        store: 'MediaTypesFORDAMPicker'
+                        type: 'list',
+                        labelField: 'type',
+                        store: Ext.getStore('MediaTypesFORDAMPicker')
                     },
                     dataIndex: 'typeId',
 
@@ -177,11 +175,9 @@ Ext.define('Rubedo.view.ImagePickerWindow', {
                         }
                     },
                     filter: {
-                        type: 'combo',
-                        valueField: 'id',
-                        displayField: 'type',
-                        id:'chooseMTInPickerCombo',
-                        store: 'MediaTypesFORDAMPicker'
+                        type: 'list',
+                        labelField: 'type',
+                        store: Ext.getStore('MediaTypesFORDAMPicker')
                     },
                     dataIndex: 'typeId',
 
@@ -208,10 +204,9 @@ Ext.define('Rubedo.view.ImagePickerWindow', {
                     }
                 },
                 filter: {
-                    type: 'combo',
-                    valueField: 'id',
-                    displayField: 'type',
-                    store: 'MediaTypesFORDAMPicker'
+                    type: 'list',
+                    labelField: 'type',
+                    store: Ext.getStore('MediaTypesFORDAMPicker')
                 },
                 dataIndex: 'typeId',
 
@@ -221,103 +216,8 @@ Ext.define('Rubedo.view.ImagePickerWindow', {
             Ext.getStore("DAMPickerStore").getProxy().extraParams.tFilter="[{\"property\":\"typeId\",\"operator\":\"$in\",\"value\":"+Ext.JSON.encode(allowedTypes)+"}]";
             Ext.getStore("DAMPickerStore").load();
         }
-        var DAMPicker = Ext.widget("DAMMainView", {id:"DAMPickerView", store:Ext.getStore("DAMPickerStore"),columns:columnsOver, multiSelect:false, plugins:[
-            Ext.create('Ext.ux.grid.FilterBar', {renderHidden: false, showShowHideButton: true,showClearAllButton: true})
-
-            ], features: [Ext.create('Ext.ux.grid.feature.Tileview', {
-                viewMode: 'tileIcons',
-                getAdditionalData: function(data, index, record, orig)
-                {
-
-
-
-                    generateThumbnail = function()
-                    {
-                        return "dam/get-thumbnail?id="+record.get("id");
-                    };
-
-                    if(this.viewMode)
-                    {
-                        return {
-                            thumbnails: generateThumbnail(),
-                            author:record.get("createUser").fullName,
-                            date: Ext.Date.format(record.get("createTime"), 'd-m-Y'),
-                            filename:record.get("title"),
-                            fileSize:Ext.util.Format.fileSize(record.get("fileSize"))
-                        };
-                    }
-                    return {};
-                },
-                viewTpls:
-                {
-                    mediumIcons: [
-                    '<td class="{cls} ux-explorerview-medium-icon-row">',
-                    '<table class="x-grid-row-table">',
-                    '<tbody>',
-                    '<tr>',
-                    '<td class="x-grid-col x-grid-cell ux-explorerview-icon" style="background: transparent;">',
-                    '<img src=\"{thumbnails}\" height=\"100\" width=\"100\">',			
-                    '</td>',
-                    '</tr>',
-                    '<tr>',
-                    '<td class="x-grid-col x-grid-cell">',
-                    '<div class="x-grid-cell-inner" unselectable="on">{filename}</div>',
-                    '</td>',
-                    '</tr>',
-                    '</tbody>',
-                    '</table>',
-                    '</td>'].join(''),
-
-                    tileIcons: [
-                    '<td class="{cls} ux-explorerview-detailed-icon-row">',
-                    '<table class="x-grid-row-table">',
-                    '<tbody>',
-                    '<tr>',
-                    '<td class="x-grid-col x-grid-cell ux-explorerview-icon" style="background: transparent;">',
-                    '<img src=\"{thumbnails}\" height=\"50\" width=\"50\">',			
-                    '</td>',
-
-                    '<td class="x-grid-col x-grid-cell">',
-                    '<div class="x-grid-cell-inner" unselectable="on">{filename}<br><span>{fileSize}<br>{author}<br>{date}</span></div>',
-                    '</td>',
-                    '</tr>',
-                    '</tbody>',
-                    '</table>',
-                    '</td>'].join('')
-
-                }
-            }),
-            {
-                ftype: 'grouping',
-                groupHeaderTpl: '{name} ({rows.length} Item{[values.rows.length > 1 ? "s" : ""]})',
-                disabled: false
-            }],
-            tbar: [{},'->', {
-            xtype: 'switchbuttonsegment',
-            activeItem: 1,
-            scope: this,
-            items: [{
-                tooltip: 'Details',
-                viewMode: 'default',
-                iconCls: 'icon-default'
-            }, {
-                tooltip: 'Tiles',
-                viewMode: 'tileIcons',
-                iconCls: 'icon-tile'
-            }, {
-                tooltip: 'Icons',
-                viewMode: 'mediumIcons',
-                iconCls: 'icon-medium'
-            }],
-            listeners: {
-                change: function(btn, item)
-                {
-                    btn.up().up().up().features[0].setView(btn.viewMode);		
-                },
-                scope: this
-            }
-        }
-    ]});
+        var DAMPicker = Ext.widget("DAMMainView", {features:[Ext.create("Ext.ux.grid.FiltersFeature",{encode:true,local:false})],id:"DAMPickerView", store:Ext.getStore("DAMPickerStore"),columns:columnsOver, multiSelect:false
+    });
     DAMPicker.on("selectionchange", function(g, s){
         if (Ext.isEmpty(s)){
             Ext.getCmp("imagePickerAcceptBtn").disable();
