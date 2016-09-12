80cb7ed44b87d9a85f5ef07d67a629d98db155e1
Alexandru Dobre <alexandru.dobre@webtales.fr>
2016-09-12 17:00:41 +0200

    WIP items layout

--- www/resources/cauldron/js/app.js
+++ www/resources/cauldron/js/app.js
@@ -1,4 +1,41 @@
 var App = angular.module('cauldron', ['ngDragDrop']);
 App.controller('CauldronController', ['$scope',function($scope) {
+    var me=this;
+    me.elementSet=[
+        {
+            title:"If ... Then ...",
+            icon:"next.png"
+        },
+        {
+            title:"When ... DO ...",
+            icon:"flag.png"
+        },
+        {
+            title:"Set variable",
+            icon:"page_next.png"
+        },
+        {
+            title:"Increment variable",
+            icon:"add.png"
+        },
+        {
+            title:"Decrement variable",
+            icon:"remove.png"
+        },
+        {
+            title:"Show modal",
+            icon:"windows.png"
+        },
+        {
+            title:"Show block",
+            icon:"application_add.png"
+        },
+        {
+            title:"Hide block",
+            icon:"application_remove.png"
+        }
+    ];
+    me.instructionsArray=[
 
+    ];
 }]);
